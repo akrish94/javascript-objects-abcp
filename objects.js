186acb121904e9671b2518cac66655473f562bd4
Adithi Krishnamoorthy <adithikrishnamoorthy@gmail.com>
2018-01-15 21:15:29 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,10 @@
+var playlist = { 
+  "Taylor Swift": "Enchanted" 
+  "Kanye West": "Homecoming"
+}
+
+function updatePlaylist(playlist, artist, song) {
+  playlist[artist] = song
+  return playlist
+}
+
